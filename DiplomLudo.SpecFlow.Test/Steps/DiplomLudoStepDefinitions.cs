b7318e9639e5f000e440804cdf39f7dc06190850
Peter Østergaard <peter.oestergaard@gls-denmark.com>
2023-04-25 12:52:08 +0200

    Scenarios

--- DiplomLudo.SpecFlow.Test/Steps/DiplomLudoStepDefinitions.cs
+++ DiplomLudo.SpecFlow.Test/Steps/DiplomLudoStepDefinitions.cs
@@ -5,11 +5,25 @@ public class DiplomLudoStepDefinitions
 {
     private int _numberOfPlayers;
     private Game _game;
+    private Player _firstPlayer;
+    private Player _secondPlayer;
 
     [Given(@"the number of players is (.*)")]
     public void GivenTheNumberOfPlayersIs(int numPlayer)
     {
         _numberOfPlayers = numPlayer;
+    }
+    
+    [Given(@"the first player is yellow")]
+    public void GivenTheFirstPlayerIsYellow()
+    {
+        _firstPlayer = new Player(Color.Yellow);
+    }
+    
+    [Given(@"the second player is red")]
+    public void GivenTheSecondPlayerIsRed()
+    {
+        _secondPlayer = new Player(Color.Red);
     }
 
     [When(@"the game is started")]
@@ -18,9 +32,4 @@ public class DiplomLudoStepDefinitions
         _game = new Game(_numberOfPlayers);
     }
 
-    [Then(@"the gameboard will be empty")]
-    public void ThenTheGameboardWillBeEmpty()
-    {
-        _game.Board.Tiles.
-    }
 }
